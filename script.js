b4ed77645b51dcd96e8d49ef2e5d059276112a8e
krishna jha <110991571+krishnajha97@users.noreply.github.com>
2023-01-20 22:25:55 +0530

    added index.html

---
+++ script.js
@@ -0,0 +1,21 @@
+
+let string = "";
+
+let buttons = document.querySelectorAll('.button1,.button2,.button3,.button4,.button5,.button6');
+Array.from(buttons).forEach((button)=>{
+  button.addEventListener('click', (e)=>{
+    if(e.target.innerHTML == '='){
+      string = eval(string);
+      document.querySelector('input').value = string;
+    }
+    else if(e.target.innerHTML == 'C'){
+      string = ""
+      document.querySelector('input').value = string;
+    }
+    else{ 
+    console.log(e.target)
+    string = string + e.target.innerHTML;
+    document.querySelector('input').value = string;
+      }
+  })
+})
